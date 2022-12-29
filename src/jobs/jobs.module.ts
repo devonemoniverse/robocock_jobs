b33e42d8b8186d08877794e2ec69d43f2088d468
argie gaviola <argie.gaviola@synerteqsys.com>
2022-12-30 00:19:38 +0800

    added enable of release crystal job

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -8,6 +8,7 @@ import { DepositWithdrawJobService } from "./deposit-withdraw-job/deposit-withdr
 import { MockService } from "./mock.service";
 import { RobocockNftJobService } from "./nft-job/robocock-nft-job.service";
 import { CancelExpiredPveService } from "./store-proc-job/cancel_expired_pve.service";
+import { ReleaseCrystalService } from "./store-proc-job/release-crystal.service";
 import { TransferService } from "./transfer-job/transfer.service";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -25,7 +26,8 @@ require("dotenv").config();
     process.env.ENABLE_CONVERSION_JOB === Constants.YES ? ConversionJobService: MockService,
     process.env.ENABLE_DEPOSIT_WITHDRAW_JOB === Constants.YES ? DepositWithdrawJobService : MockService,
 
-    process.env.ENABLE_STORE_PROC_JOB === Constants.YES ? CancelExpiredPveService : MockService
+    process.env.ENABLE_STORE_PROC_JOB === Constants.YES ? CancelExpiredPveService : MockService,
+    process.env.ENABLE_STORE_PROC_JOB === Constants.YES ? ReleaseCrystalService : MockService
  
   ],
 })
